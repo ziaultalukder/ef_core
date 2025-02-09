29193a031411d1f8f4436545fd7133530b2f9d64
ziaul2023 <ziaul@primotechltd.com>
2025-02-09 18:31:54 +0600

    modify student controller

--- EntityFrameWorkWithCore/Controllers/StudentController.cs
+++ EntityFrameWorkWithCore/Controllers/StudentController.cs
@@ -51,5 +51,11 @@ namespace EntityFrameWorkWithCore.Controllers
         {
             throw new NotImplementedException("this method is not implement");
         }
+        
+        [HttpGet("[action]")]
+        public async Task<IActionResult> MethodFromPatch1Brach()
+        {
+            throw new NotImplementedException("MethodFromPatch1Brach");
+        }
     }
 }
